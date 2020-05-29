3ad703b11a43e38d2e4a68b619065b0829d75cf6
Rasika Deodhar <rasikadeodhar95@gmail.com>
2020-05-29 15:20:37 +0100

    Assignment 2

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
 class Main extends Component {
@@ -51,6 +52,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route path='/about' component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
